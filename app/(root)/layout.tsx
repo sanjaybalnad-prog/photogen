2eb4f8dd0fcaa15a34df4e60e72b1f8dd28f800b
Abhinav-c2004 <abhinav.kanan007@gmail.com>
2026-03-24 12:46:04 +0530

    Update layout.tsx
    feat: used mobile nav in home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import SideBar from '@/components/SideBar'
+import MobileNav from '@/components/MobileNav'
 import React from 'react'
 
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <main className="root">
             <SideBar />
+            <MobileNav />
             <div className="root-container">
                 <div className="wrapper">{children}</div>
             </div>
