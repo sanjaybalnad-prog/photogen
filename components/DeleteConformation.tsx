f8f789f65fa1fe95bfdd77da316d3a7715517837
Abhinav-c2004 <abhinav.kanan007@gmail.com>
2026-04-04 21:27:06 +0530

    feat: Added DeleteConformation model

---
+++ components/DeleteConformation.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useTransition } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { deleteImage } from "@/actions/image.action";
+
+import { Button } from "./ui/button";
+
+export const DeleteConfirmation = ({ imageId }: { imageId: string }) => {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild className="w-full rounded-full">
+        <Button type="button" className="h-[44px] w-full md:h-[54px] cursor-pointer" variant="destructive">
+          Delete Image
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent className="flex flex-col gap-10">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to delete this image?</AlertDialogTitle>
+          <AlertDialogDescription className="p-16-regular">
+            This will permanently delete this image
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="border bg-red-500 text-secondary hover:bg-red-600"
+            onClick={() =>
+              startTransition(async () => {
+                await deleteImage(imageId);
+              })
+            }
+          >
+            {isPending ? "Deleting..." : "Delete"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
