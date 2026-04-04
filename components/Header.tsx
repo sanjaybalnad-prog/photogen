e2bdfea4cd0936a7d02a90cedd4c57b98050c9bb
Abhinav-c2004 <abhinav.kanan007@gmail.com>
2026-04-04 21:17:01 +0530

    feat:Implemented header component for future use

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header({ title, subtitle }: { title: string; subtitle?: string }) {
+  return (
+    <>
+      <h2 className="h2-bold text-primary">{title}</h2>
+      {subtitle && <p className="p-16-regular mt-4">{subtitle}</p>}
+    </>
+  );
+}
